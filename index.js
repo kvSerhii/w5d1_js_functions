7c111abf91aaa4416eb5f762eb96f7931ecf2cac
Serhii <kv.serhii@gmail.com>
2020-08-08 19:51:34 +0300

    W5d1: JS functions
    	add function constructor
    
    	changed	files and directories
    		- index.js

--- index.js
+++ index.js
@@ -79,3 +79,105 @@ function isEven(number) {
   return number % 2 === 0;
 } */
 
+/* // objects
+const cat = {
+  color: "grey",
+  age: 2,
+  weight: 3,
+  male: true,
+  breed: "Abyssinian",
+  catties: {
+    amount: 3,
+  },
+  sayHi() {
+    return "Meow";
+  },
+  run() {
+    return "Cat is running";
+  },
+  eat() {
+    return "Cat eating";
+  },
+};
+
+console.log(cat.sayHi());
+console.log(cat.run());
+console.log(cat.eat());
+
+const table = {
+  color: "darkBrown",
+  squareSize: 1.2,
+  style: "classic",
+};
+
+console.log(table.color);
+ */
+
+// this
+/* 
+function Cat(color, weight, bread, age, name) {
+  this.color = color;
+  this.weight = weight;
+  this.bread = bread;
+  this.age = age;
+  this.name = name;
+  this.intro = function () {
+    return `My name is ${this.name}`;
+  };
+}
+
+const cat1 = new Cat("white", 2300, "Burmese", 2, "Mike");
+const cat2 = new Cat("Red", 3200, "Abyssinian", 2, "Dan");
+const cat3 = new Cat("Grey", 2600, "Himalayan", 2, "Will");
+const cat4 = new Cat("Black", 3600, "Persian", 2, "Bill");
+
+console.log(cat1.intro());
+console.log(cat2.intro());
+console.log(cat3.intro());
+console.log(cat4.intro());
+ */
+
+/* function Country(name, population, square, populationDensity) {
+  this.name = name;
+  this.population = population;
+  this.square = square;
+
+  this.getDensity = function () {
+    return (this.population / this.square) * 1000000;
+  };
+}
+
+const country1 = new Country("Italy", 45, 500);
+const country2 = new Country("Spain", 78, 420);
+const country3 = new Country("Fiji", 13, 127);
+const country4 = new Country("Australia", 112, 1243);
+
+console.log(country1.populationDensity());
+console.log(country2.populationDensity());
+console.log(country3.populationDensity());
+console.log(country4.populationDensity()); */
+
+function Car(speed) {
+  this.speed = speed;
+  this.stage = function (speed) {
+    if (speed > 0) {
+      return "ride";
+    } else {
+      return "stand";
+    }
+  };
+}
+
+const car1 = new Car(this.speed);
+const car2 = new Car(this.speed);
+const car3 = new Car(this.speed);
+const car4 = new Car(this.speed);
+const car5 = new Car(this.speed);
+const car6 = new Car(this.speed);
+
+console.log(car1.stage(0));
+console.log(car2.stage(10));
+console.log(car3.stage(0));
+console.log(car4.stage(50));
+console.log(car5.stage(100));
+console.log(car6.stage(200));
